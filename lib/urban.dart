23ef1b7336dec2364ec28fb3c878d1f8ded27b9a
Moose2002 <mumua@umich.edu>
2024-01-28 05:16:09 -0500

    update

--- lib/urban.dart
+++ lib/urban.dart
@@ -1,9 +1,31 @@
 import 'package:flutter/material.dart';
 
-class UrbanPage extends StatelessWidget {
+class UrbanPage extends StatefulWidget {
+  @override
+  _UrbanPageState createState() => _UrbanPageState();
+}
+
+class _UrbanPageState extends State<UrbanPage> {
+  List<String> availableImages = [
+    'lib/bin/EV.svg',
+    'lib/bin/factory.svg',
+    'lib/bin/gascar.png',
+    'lib/bin/hydcar.png',
+    'lib/bin/streetlight.svg',
+    'lib/bin/trash-bin.png',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+      ),
+      drawer: AppDrawer(
+        availableImages: availableImages,
+        onImageSelected: (String imagePath) {
+          // Implement your logic for image selection here
+        },
+      ),
       body: Container(
         width: double.infinity,
         height: double.infinity,
@@ -17,13 +39,7 @@ class UrbanPage extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text(
-                "",
-                style: TextStyle(
-                  fontSize: 24,
-                  color: Colors.white,
-                ),
-              ),
+
               // Add more widgets as needed
             ],
           ),
@@ -31,7 +47,7 @@ class UrbanPage extends StatelessWidget {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          Navigator.pop(context); // This line will navigate back to the previous screen
+          Navigator.pop(context); // Navigates back to the previous screen
         },
         child: Icon(Icons.home), // Icon for the button
         tooltip: 'Go back to home', // Tooltip text on long press
@@ -39,3 +55,33 @@ class UrbanPage extends StatelessWidget {
     );
   }
 }
+
+class AppDrawer extends StatelessWidget {
+  final List<String> availableImages;
+  final Function(String) onImageSelected;
+
+  AppDrawer({required this.availableImages, required this.onImageSelected});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: availableImages.map((imagePath) {
+          // Extracting the image name from the path for displaying as text
+          var imageName = imagePath.split('/').last.split('.').first;
+
+          return ListTile(
+            leading: Image.asset(
+              imagePath,
+              width: 40.0,
+              height: 40.0,
+            ),
+            title: Text(imageName), // Display the image name here
+            onTap: () => onImageSelected(imagePath),
+          );
+        }).toList(),
+      ),
+    );
+  }
+}
+
